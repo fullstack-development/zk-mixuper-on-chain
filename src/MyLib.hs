d3708259f54efd914e7dd532806859bce379c269
stanislav-az <st.zhdanovich@gmail.com>
2023-05-18 15:34:23 +0700

    fun

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,12 @@
-module MyLib (someFunc) where
+module MyLib where
+
+import Plutarch.Prelude
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+pfun :: Term s (a :--> b)
+pfun = undefined
+
+hfun :: Term s a -> Term s b
+hfun x = pfun # x
