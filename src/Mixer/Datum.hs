82be5c0fe25350a72ab894d7ffab99a9e13cd39c
Stanislav Zhdanovich <st.zhdanovich@gmail.com>
2023-05-22 17:44:37 +0700

    mixer config

--- src/Mixer/Datum.hs
+++ src/Mixer/Datum.hs
@@ -1,15 +1,19 @@
 module Mixer.Datum where
 
 import Plutarch.Prelude
+import Plutarch.Api.V2 (PTuple, PCurrencySymbol, PTokenName)
+import Plutarch.DataRepr (PDataFields)
 
 -- | A type for representing hash digests.
-type Hash = PByteString
+type PHash = PByteString
 
 -- | A sha-256 digest of (nullifier <> secret)
-type Commitment = Hash
+type PCommitment = PHash
+
+type Lovelace = PInteger
 
 data MixerRedeemer (s :: S)
-  = Deposit (Term s (PDataRecord '["commitment" := Commitment]))
+  = Deposit (Term s (PDataRecord '["commitment" := PCommitment]))
   | Withdraw (Term s (PDataRecord '[]))
   -- TODO(?) Close redeemer for pool operator / creator (fixed pkh)
   -- Close (Term s (PDataRecord '[]))
@@ -17,4 +21,15 @@ data MixerRedeemer (s :: S)
   deriving anyclass (PlutusType, PIsData)
 
 instance DerivePlutusType MixerRedeemer where
+  type DPTStrat _ = PlutusTypeData
+
+-- | An asset class, identified by a CurrencySymbol and a TokenName. PAssetClass :: PType
+type PAssetClass = PTuple PCurrencySymbol PTokenName
+
+newtype MixerConfig (s :: S)
+  = MixerConfig (Term s (PDataRecord '["protocolToken" := PAssetClass, "poolNominal" := Lovelace]))
+  deriving stock (Generic)
+  deriving anyclass (PlutusType, PDataFields, PIsData)
+
+instance DerivePlutusType MixerConfig where
   type DPTStrat _ = PlutusTypeData
