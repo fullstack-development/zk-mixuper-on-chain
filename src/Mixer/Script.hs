7baa8ffb96538ad3e935ab9fdcfd1d0e84818938
stanislav-az <st.zhdanovich@gmail.com>
2023-05-23 13:30:10 +0700

    main validator branching

--- src/Mixer/Script.hs
+++ src/Mixer/Script.hs
@@ -1,1 +1,40 @@
 module Mixer.Script where
+
+import Mixer.Datum (MixerConfig, MixerRedeemer (..), PCommitment)
+import Plutarch.Api.V2 (
+  PDatum,
+  PScriptContext,
+  PScriptPurpose (PSpending),
+  PTxInfo,
+ )
+import Plutarch.DataRepr (HRec, HRecOf, PMemberFields)
+import qualified Plutarch.Monadic as P
+import Plutarch.Prelude
+
+validatorLogic ::
+  Term
+    s
+    ( PAsData MixerConfig
+        :--> PAsData PDatum
+        :--> PAsData MixerRedeemer
+        :--> PAsData PScriptContext
+        :--> PUnit
+    )
+validatorLogic = plam $ \conf d (pfromData -> r) ctx' -> P.do
+  ctx <- pletFields @["txInfo", "purpose"] ctx'
+  PSpending _ <- pmatch ctx.purpose
+  info <- pletFields @'["inputs", "outputs", "mint", "datums", "signatories"] $ ctx.txInfo
+  pmatch r \case
+    Deposit c -> unTermCont do
+      let commit = pfield @"commitment" # c
+      validateDeposit info commit
+    _ -> ptraceError "Not implemented"
+
+validateDeposit ::
+  PMemberFields PTxInfo '["inputs", "outputs", "mint", "datums"] s txInfo =>
+  HRec txInfo ->
+  Term s PCommitment ->
+  TermCont s (Term s PUnit)
+validateDeposit _info commit = do
+  -- Do checks with info fields here.
+  pure $ pconstant ()
