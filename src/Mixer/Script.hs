4e4255dab95d3168493402f012ae8fc01506f2fe
Stanislav Zhdanovich <st.zhdanovich@gmail.com>
2023-05-25 16:31:17 +0700

    deposit validator

--- src/Mixer/Script.hs
+++ src/Mixer/Script.hs
@@ -3,6 +3,7 @@ module Mixer.Script where
 import Ext.Plutarch.Api.V2.Contexts (inlineDatumFromOutput, pfindOwnInput, pgetContinuingOutputs, pgetOnlyOneOutputFromList)
 import Ext.Plutarch.Api.V2.Value (SortedPositiveValue)
 import Mixer.Datum (MixerConfig, MixerDatum (..), MixerRedeemer (..), PCommitment)
+import Plutarch.Api.V1.Value (plovelaceValueOf)
 import Plutarch.Api.V2 (
   PDatum,
   PScriptContext,
@@ -11,6 +12,7 @@ import Plutarch.Api.V2 (
   PTxOut,
  )
 import Plutarch.DataRepr (HRec, HRecOf, PMemberFields)
+import Plutarch.Extra.TermCont (pguardC)
 import qualified Plutarch.Monadic as P
 import Plutarch.Prelude
 
@@ -24,7 +26,7 @@ validatorLogic ::
         :--> PAsData PScriptContext
         :--> PUnit
     )
-validatorLogic = plam $ \(pfromData -> conf) (pfromData -> d) (pfromData -> r) ctx' -> P.do
+validatorLogic = plam \(pfromData -> config) (pfromData -> d) (pfromData -> r) ctx' -> P.do
   ctx <- pletFields @["txInfo", "purpose"] ctx'
   info <- pletFields @'["inputs", "outputs", "mint", "datums", "signatories"] $ ctx.txInfo
   -- Find own input:
@@ -37,24 +39,31 @@ validatorLogic = plam $ \(pfromData -> conf) (pfromData -> d) (pfromData -> r) c
     -- Get produced datum:
     (nextState, _) <- ptryFrom @MixerDatum $ inlineDatumFromOutput # ownOutput.datum
     outputState <- pletFields @'["nullifierHashes"] nextState
-    -- Allowed transitions given a redeemer:
-    plet (ownOutput.value) $ \outputValue -> P.do
+    plet (ownOutput.value) \outputValue -> P.do
+      -- TODO Check protocol token
+      conf <- pletFields @'["protocolToken", "poolNominal"] config
       let inputValue = pfield @"value" # ownInputResolved
       inputState <- pletFields @'["nullifierHashes"] d
+      -- Allowed transitions given a redeemer:
       pmatch r \case
         Deposit c -> unTermCont do
           let commit = pfield @"commitment" # c
-          validateDeposit inputState outputState inputValue outputValue commit
+          validateDeposit conf inputState outputState inputValue outputValue commit
         _ -> ptraceError "Not implemented"
 
 validateDeposit ::
-  PMemberFields MixerDatum '["nullifierHashes"] s datum =>
+  ( PMemberFields MixerDatum '["nullifierHashes"] s datum
+  , PMemberFields MixerConfig '["poolNominal"] s config
+  ) =>
+  HRec config ->
   HRec datum ->
   HRec datum ->
   Term s SortedPositiveValue ->
   Term s SortedPositiveValue ->
   Term s PCommitment ->
   TermCont s (Term s PUnit)
-validateDeposit inputState outputState inputValue outputValue commit = do
-  -- Do checks with info fields here.
+validateDeposit conf inputState outputState inputValue outputValue commit = do
+  pguardC "Nullifier hash list modified" (inputState.nullifierHashes #== outputState.nullifierHashes)
+  let depositedAmount = (plovelaceValueOf # outputValue) - (plovelaceValueOf # inputValue)
+  pguardC "Nominal amount should be paid to script" (depositedAmount #== conf.poolNominal)
   pure $ pconstant ()
