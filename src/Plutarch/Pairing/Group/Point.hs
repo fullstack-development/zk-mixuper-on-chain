1c6f8525df4169345c31dcbb256abdf9ab483226
stanislav-az <st.zhdanovich@gmail.com>
2023-06-30 22:07:11 +0700

    point multiplication in jacobian coordinates

--- src/Plutarch/Pairing/Group/Point.hs
+++ src/Plutarch/Pairing/Group/Point.hs
@@ -2,14 +2,17 @@ module Plutarch.Pairing.Group.Point where
 
 import Ext.Plutarch.Num (peven)
 import Plutarch.Api.V2 (PTuple)
+import Plutarch.DataRepr (PDataFields)
 import qualified Plutarch.Monadic as P
 import Plutarch.Num (PNum (..))
+import Plutarch.Pairing.BN128 (_a)
 import Plutarch.Pairing.Group.Class (
   PGroup (..),
   PMonoid (..),
   PSemigroup (..),
  )
 import Plutarch.Prelude
+import qualified Plutus.Pairing.BN128 as Plutus
 import qualified PlutusTx.Monoid as PlutusTx
 import qualified PlutusTx.Prelude as PlutusTx
 
@@ -94,6 +97,12 @@ pgNeg' = phoistAcyclic $ plam
 pgMul :: (PIsData a, PNum a, PGroup a) => Term s (PPoint a :--> PInteger :--> PPoint a)
 pgMul = phoistAcyclic $
   plam $
+    \pt n -> pfromJ' #$ pmulJ # ptoJ pt # n
+
+-- | Multiplication by a scalar
+pgMulHeavy :: (PIsData a, PNum a, PGroup a) => Term s (PPoint a :--> PInteger :--> PPoint a)
+pgMulHeavy = phoistAcyclic $
+  plam $
     \pt n ->
       pif (n #< 0) (ptraceError "pgMul: negative scalar not supported") $
         pif (n #== 0) pInfinity $
@@ -127,3 +136,116 @@ instance (PIsData a, PNum a, PGroup a) => PMonoid (PPoint a) where
 
 instance (PIsData a, PNum a, PGroup a) => PGroup (PPoint a) where
   pinv = pgNeg
+
+--------------------- Jacobian coordinates -----------------------------
+newtype PPointJ (a :: PType) (s :: S)
+  = PPointJ (Term s (PDataRecord '["x" := a, "y" := a, "z" := a]))
+  deriving stock (Generic)
+  deriving anyclass (PlutusType, PIsData, PDataFields, PEq, PShow)
+
+instance DerivePlutusType (PPointJ a) where
+  type DPTStrat _ = PlutusTypeData
+
+instance (PTryFrom PData a) => PTryFrom PData (PPointJ a)
+
+-- instance (PTryFrom PData (PAsData a)) => PTryFrom PData (PAsData (PPointJ a))
+
+pPointJ :: (PIsData a) => Term s a -> Term s a -> Term s a -> Term s (PPointJ a)
+pPointJ x y z =
+  pcon $
+    PPointJ $
+      pdcons @"x"
+        # pdata x
+          #$ pdcons @"y"
+        # pdata y
+          #$ pdcons @"z"
+        # pdata z
+        # pdnil
+
+ptoJ :: (PIsData a, PNum a) => Term s (PPoint a) -> Term s (PPointJ a)
+ptoJ pt = pmatch pt \case
+  PInfinity _ -> pPointJ 1 1 0
+  PPoint pr -> P.do
+    p <- pletFields @'["x", "y"] pr
+    pPointJ p.x p.y 1
+
+pfromJ :: (PIsData a, PNum a, PGroup a) => Term s (PPointJ a) -> Term s (PPoint a)
+pfromJ pt = pfromJ' # pt
+
+pfromJ' :: (PIsData a, PNum a, PGroup a) => Term s (PPointJ a :--> PPoint a)
+pfromJ' = phoistAcyclic $
+  plam $
+    \pt -> P.do
+      p <- pletFields @'["x", "y", "z"] pt
+      pif (p.z #== pdata 0) pInfinity P.do
+        zz <- plet $ p.z * p.z
+        pPoint (p.x * pinv zz) (p.y * pinv (p.z * zz))
+
+pinvJ :: (PIsData a, PNum a) => Term s (PPointJ a :--> PPointJ a)
+pinvJ = phoistAcyclic $
+  plam $
+    \pt -> P.do
+      p <- pletFields @'["x", "y", "z"] pt
+      pPointJ p.x (pnegate # p.y) p.z
+
+pdoubleJ :: forall s a. (PIsData a, PNum a) => Term s (PPointJ a :--> PPointJ a)
+pdoubleJ = phoistAcyclic $
+  plam $
+    \pt -> P.do
+      p <- pletFields @'["x", "y", "z"] pt
+      one <- plet $ pfromInteger 1
+      a <- plet $ pfromInteger Plutus._a
+      two <- plet $ one + one
+      three <- plet $ two + one
+      eight <- plet $ three + three + two
+      xx <- plet $ p.x * p.x
+      yy <- plet $ p.y * p.y
+      yyyy <- plet $ yy * yy
+      zz <- plet $ p.z * p.z
+      xy <- plet $ p.x + yy
+      yz <- plet $ p.y + p.z
+      s <- plet $ two * (xy * xy - xx - yyyy)
+      m <- plet $ three * xx + a * zz * zz
+      t <- plet $ m * m - two * s
+      y3 <- plet $ m * (s - t) - eight * yyyy
+      z3 <- plet $ yz * yz - yy - zz
+      pPointJ t y3 z3
+
+paddJ :: forall s a. (PIsData a, PNum a) => Term s (PPointJ a :--> PPointJ a :--> PPointJ a)
+paddJ = phoistAcyclic $
+  plam $
+    \pt qt -> P.do
+      p <- pletFields @'["x", "y", "z"] pt
+      q <- pletFields @'["x", "y", "z"] qt
+      pif (p.z #== pdata 0) (pPointJ q.x q.y q.z) $
+        pif (q.z #== pdata 0) (pPointJ p.x p.y p.z) $
+          pif (p.x #== q.x) (pPointJ 1 1 0) P.do
+            one <- plet $ pfromInteger 1
+            two <- plet $ one + one
+            z1z1 <- plet $ p.z * p.z
+            z2z2 <- plet $ q.z * q.z
+            z1z2 <- plet $ p.z + q.z
+            u1 <- plet $ p.x * z2z2
+            u2 <- plet $ q.x * z1z1
+            s1 <- plet $ p.y * q.z * z2z2
+            s2 <- plet $ q.y * p.z * z1z1
+            h <- plet $ u2 - u1
+            h2 <- plet $ two * h
+            i <- plet $ h2 * h2
+            j <- plet $ h * i
+            r <- plet $ two * (s2 - s1)
+            v <- plet $ u1 * i
+            x3 <- plet $ r * r - j - two * v
+            y3 <- plet $ r * (v - x3) - two * s1 * j
+            z3 <- plet $ (z1z2 * z1z2 - z1z1 - z2z2) * h
+            pPointJ x3 y3 z3
+
+pmulJ :: forall s a. (PIsData a, PNum a) => Term s (PPointJ a :--> PInteger :--> PPointJ a)
+pmulJ =
+  phoistAcyclic $
+    pfix #$ plam \self pt n ->
+      pif (n #< 0) (pinvJ #$ self # pt # (pnegate # n)) $
+        pif (n #== 0) (pPointJ 1 1 0) $
+          pif (n #== 1) pt P.do
+            p' <- plet $ pmulJ # (pdoubleJ # pt) # (pdiv # n # 2)
+            pif (peven # n) p' (paddJ # pt # p')
